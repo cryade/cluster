296f15ae1e1164acfc6b5054296f5ba5b068b670
cryade <48327326+cryade@users.noreply.github.com>
2019-04-18 14:36:16 +0200

    Create data_server.js

---
+++ data_server.js
@@ -0,0 +1,66 @@
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8081 });
+
+const wsarray = [];
+var ind = 100000;
+
+wss.on('connection', function connection(ws) {
+  console.log("Client Connected");
+  wsarray.push(ws);
+
+  ws.on('close', function close() {
+    clearInterval(interval);
+    wsarray.splice(wsarray.indexOf(ws), 1);
+    console.log('Client disconnected');
+  })
+
+  ws.on('message', function print(msg) {
+    console.log(msg);
+  })
+  
+
+  var interval;
+
+  interval = setInterval(() => {
+    
+    let objString = JSON.stringify(createRandomData()); 
+    wsarray.forEach(ws => ws.send(objString));
+    ind++;
+  }, 10000)
+});
+
+function nokOK(){
+  if(Math.random()>0.95){
+    return "NOK"
+  }
+  else return "OK"
+}
+
+function createRandomData(){
+  let obj= {
+    "Time" : Date(),
+    "Audit":
+    {  
+      "ID" : ind,
+      "Components" : 
+        {
+          "Battery" : (Math.random()>0.5),
+          "Fan1" : (Math.random()>0.5),
+          "Fan2" : (Math.random()>0.5),
+          "Fan3" : (Math.random()>0.5),
+          "Fan4" : (Math.random()>0.5),
+          "DIMM5" : (Math.random()>0.5),
+          "DIMM6" : (Math.random()>0.5),
+          "DIMM7" : (Math.random()>0.5),
+          "SATA1" : (Math.random()>0.5),
+          "SATA2" : (Math.random()>0.5),
+        }
+        
+    },
+    "Evaluation" : (Math.random()>0.5), 
+    }
+    
+
+    obj._id=obj.Audit.ID;
+    return obj;
+  }
